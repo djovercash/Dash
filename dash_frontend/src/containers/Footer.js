ca50734a0ec5446e3b8e8af01a3d38c762ab85d4
github username <github email address>
2018-03-26 17:16:51 -0400

    Footer Materials Added

--- dash_frontend/src/containers/Footer.js
+++ dash_frontend/src/containers/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import moment from 'moment-timezone'
 
 const Footer = () => {
+  const year = moment(Date.now()).format("YYYY")
   return (
     <div id="footer">
-      <h3></h3>
+      <div id="footerCopy">
+        <h5>&copy;DASH {year} All rights reserved</h5>
+      </div>
+      <div>
+        <nav className="footerNav">
+          <ul>
+            <li><a href="#"><i><img src="FB.png" /></i></a></li>
+            <li><a href="#"><i><img src="Logos-Twitter-icon.png" /></i></a></li>
+            <li><a href="#"><i><img src="Instgram.png" /></i></a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
